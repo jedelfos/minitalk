0d65644bf4547e3dc311ea04f7eda0ef2d5b0017
Jerome  Delfosse <jedelfos@z3r11p3.42lyon.fr>
2021-07-21 18:10:35 +0200

    2.1 fin

--- include/client.h
+++ include/client.h
@@ -6,17 +6,17 @@
 /*   By: jedelfos <jedelfos@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 17:04:18 by jedelfos          #+#    #+#             */
-/*   Updated: 2021/07/21 18:05:17 by jedelfos         ###   ########lyon.fr   */
+/*   Updated: 2021/07/21 18:10:15 by jedelfos         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_H
 # define CLIENT_H
 
-#include <stdio.h>
-#include <signal.h> 
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <signal.h> 
+# include <unistd.h>
+# include <stdlib.h>
 
 int	ft_atoi(const char *str);
 int	erreur(int i);
--- include/server.h
+++ include/server.h
@@ -6,18 +6,18 @@
 /*   By: jedelfos <jedelfos@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 17:04:23 by jedelfos          #+#    #+#             */
-/*   Updated: 2021/07/21 18:05:19 by jedelfos         ###   ########lyon.fr   */
+/*   Updated: 2021/07/21 18:10:13 by jedelfos         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <stdlib.h> 
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <string.h>
+# include <stdlib.h> 
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
+# include <string.h>
 
 int	ft_itoa(int n);
 int	retour(int sig);
